8a3fc5ad0d2144a504fc9054669fdbc65420f042
Daniel Oshima <doshima2013@gmail.com>
2024-11-23 11:29:29 -0500

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(currentLocation) {
+
+    return Math.abs(currentLocation - 42);
+}
+
+function distanceFromHqInFeet(currentLocation) {
+   return distanceFromHqInBlocks(currentLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const ruleOne = Math.abs(destination - start) * 264;
+    if (ruleOne < 400) {
+        return 0
+    }
+    else if (400 <= ruleOne && ruleOne <= 2000) {
+        return (ruleOne - 400) * .02
+    }
+    else if (ruleOne > 2000 && ruleOne <= 2500) {
+        return 25
+    }
+    else {
+        return ("cannot travel that far")
+    }
+}
